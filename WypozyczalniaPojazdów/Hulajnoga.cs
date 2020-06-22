f175023a29b168ac1c65068e21a54087981485ce
Mardud12 <patrykpodyma24@gmail.com>
2020-06-22 21:23:12 +0200

    .

--- WypozyczalniaPojazdów/Hulajnoga.cs
+++ WypozyczalniaPojazdów/Hulajnoga.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WypozyczalniaPojazdów
+﻿namespace WypozyczalniaPojazdów
 {
     public class Hulajnoga : Pojazd
     {
